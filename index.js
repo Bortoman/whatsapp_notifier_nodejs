d65e2b6662aec430344790add2e701e870a4c859
Bortoman <bortoman0@gmail.com>
2021-06-03 16:26:21 +0200

    fixed some styling and naming for portfolio

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const puppeteer = require('puppeteer');
 const fetch = require('node-fetch');
-const FormData = require('form-data')
-const rl = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
 
 // CONFIG VARIABLES - TO CHANGE IN CASE WHATSAPP DECIDES TO CHANGE CLASSES NAMES
 const search_box_class='._1awRl';
-const chat_list_class = '.3dHYI';
+const search_box_number_input_class='._3Eocp'
 
 /*
 * How does this work?
-* When registering a user should send a message to our whatsapp
-* The user's number is saved in the contacts and we will therefore have a prev. conversation with him/her
+* The user's number is saved in the contacts therefore we will be able to start a conversation with him/her
 * The service should do a polling to the server that builds a queue of notifications
 * The notification queue is consumed by type and recipient
 * If the queue is empty the service will continue polling at a lower rate. 
-*
 */
+
 (async function main(){
     try {
         const browser = await puppeteer.launch({headless: false});
@@ -28,23 +22,23 @@ const chat_list_class = '.3dHYI';
         );
         console.log('navigating...');
         await page.goto('https://web.whatsapp.com/');
-        await page.waitForSelector('._1awRl ', {timeout: 1000000});
+        await page.waitForSelector(search_box_class, {timeout: 1000000});
         console.log('whatsapp loaded');
         var recursiveFetch = async function () {
-            var data = await fetch("http://www.noorriyadh.xyz/xhr/list-notifications?token=91286asjfb9q734f9rr7134tfo2f").then(response => {
-                            return response.json()
+            fetch("http://mydomain.com/list-notifications").then(response => {
+                    return response.json()
                 }).then(async function (json) {
-                    contacts = JSON.parse(json);
+                    notifications = JSON.parse(json);
                     console.log('fetching the value...')
-                    if (contacts.length > 0) {
-                        for (c of contacts) {
+                    if (notifications.length > 0) {
+                        for (notification of notifications) {
                             console.log('searching for the contact');
-                            await page.$('._3Eocp').then((erase)=>erase.click()).catch((error)=> console.log('nothing to erase...'));
-                            const search = await page.$('._1awRl');
+                            await page.$(search_box_number_input_class).then((erase)=>erase.click()).catch((error)=> console.log('nothing to erase...'));
+                            const search = await page.$(search_box_class);
                             await search.click();
                             console.log('pasting contact...');
-                            console.log(c.fields.number);
-                            let number = c.fields.number.toString();
+                            let number = notification.fields.number.toString();
+                            // in some countries people is used to save their number starting with 0, this is not saved in whatsapp so we will remove it
                             if (number.startsWith('0')) {
                                 number = number.slice(1,number.length);
                             }
@@ -52,11 +46,9 @@ const chat_list_class = '.3dHYI';
                             console.log('Looking for chats...');
                             await page.waitForFunction('document.querySelector("body").innerText.includes("No chats")', {timeout: 5000})
                             .then(() => {
-                                console.log(
-                                    "no contacts found..."
-                                );
-                                var code = 404;
-                                var response = fetch(`http://www.noorriyadh.xyz/xhr/consume-notification?token=91286asjfb9q734f9rr7134tfo2f&wp_pk=${c.pk}&code=${code}`).then(response => {
+                                console.log("no contacts found...");
+                                const code = 404;
+                                fetch(`http://mydomain.com/consume-notification?pk=${notification.pk}&code=${code}`).then(response => {
                                     return response.json()
                                 }).then(res => {
                                     console.log('message popped from the queue with status: ' + code);
@@ -73,11 +65,10 @@ const chat_list_class = '.3dHYI';
                                 await page.keyboard.press('Enter');
                                 setTimeout(()=> {console.log('..waited some milliseconds'), 1000});
                                 console.log('pasting message...');
-                                await page.keyboard.press('Backspace');
-                                await page.keyboard.type(c.fields.message, {delay: 10});
+                                await page.keyboard.type(notification.fields.message, {delay: 10});
                                 await page.keyboard.press('Enter');
-                                var code = 200;
-                                var response = fetch(`http://www.noorriyadh.xyz/xhr/consume-notification?token=91286asjfb9q734f9rr7134tfo2f&wp_pk=${c.pk}&code=${code}`).then(response => {
+                                const code = 200;
+                                fetch(`http://mydomain.com/consume-notification?pk=${notification.pk}&code=${code}`).then(response => {
                                     return response.json();
                                 }).then(res => {
                                     console.log('message popped from the queue with status: ' + code);
@@ -100,9 +91,7 @@ const chat_list_class = '.3dHYI';
                     setTimeout(recursiveFetch, 60000);
                 });
         } 
-
         recursiveFetch();
-        
     } catch(error) {
         console.log(error)
     }
